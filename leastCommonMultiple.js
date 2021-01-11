de7d06f4f3d051610097154167deed637464b5ad
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-11 06:23:19 +0300

    Create leastCommonMultiple.js

---
+++ leastCommonMultiple.js
@@ -0,0 +1,15 @@
+function LCM(A) {
+  let gcd1 = 0;
+  let gcd2 = 0;
+
+  for(let i = 0; i < A; i++) {
+    for(let j = n; j < A; j++) {
+      if(i % A === 0 && j % A === 0) {
+
+      }
+    }
+  }
+
+}
+
+// Compute the least common multiple of an array of integers. Given m and n, the least common multiple is the smallest positive integer that has both m and n as factors.
