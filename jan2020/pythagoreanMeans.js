c445f04edef76d177a78eb3deecf06738ac7ae6e
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-26 14:56:03 +0300

    Update pythagoreanMeans.js

--- jan2020/pythagoreanMeans.js
+++ jan2020/pythagoreanMeans.js
@@ -1,4 +1,4 @@
-// This one realliy interests me. This looks so simple, but I am trying to get my head around the formula with this function. I think I see what is happening. Simply adding up an array of numbers which are squared, and then dividing that by the length of the numbers, and then taking the square root of that. Seems simple enough. That may be good pra
+// This one realliy interests me. This looks so simple, but I am trying to get my head around the formula with this function. I think I see what is happening. Simply adding up an array of numbers which are squared, and then dividing that by the length of the numbers, and then taking the square root of that. Seems simple enough. That may be good practice to find math formulas and convert them to functions. That might be fun.
 
 function rms(arr) {
   const sumOfSquares = arr.reduce((s, x) => s + x * x, 0);
