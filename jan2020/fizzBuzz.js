8375ae1cb48bfb5d28b437feb8195a5fa313fd79
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-01 05:57:23 +0300

    Create fizzBuzz.js

---
+++ jan2020/fizzBuzz.js
@@ -0,0 +1,27 @@
+function fizzBuzz() {
+    let array = [];
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            array.push("FizzBuzz");
+        } else if (i % 5 === 0) {
+            array.push("Buzz");
+        } else if (i % 3 === 0) {
+            array.push("Fizz");
+        } else {
+            array.push(i);
+        }
+    }
+
+
+    return array;
+}
+
+fizzBuzz();
+
+// Write a program that generates an array of integers from 1 to 100 (inclusive). But:
+
+// for multiples of 3, add "Fizz" to the array instead of the number
+// for multiples of 5, add "Buzz" to the array instead of the number
+// for multiples of 3 and 5, add "FizzBuzz" to the array instead of the number
+// Your program should return an array containing the results based on the rules above.
+
