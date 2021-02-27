45e9ea75bde196c5dfcc8159462992880c3f11eb
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-27 10:38:26 +0300

    Create sha1.js

---
+++ feb2021/sha1.js
@@ -0,0 +1,15 @@
+function SHA1(input) {
+    return input;
+}
+
+console.log(
+SHA1("abc"),//"a9993e364706816aba3e25717850c26c9cd0d89d".
+
+SHA1("Rosetta Code"),//"48c98f7e5a6e736d790ab740dfc3f51a61abe2b5".
+
+SHA1("Hello world"),//"7b502c3a1f48c8609ae212cdfb639dee39673f5e".
+
+SHA1("Programming"),//"d1a946bf8b2f2a7292c250063ee28989d742cd4b".
+
+SHA1("is Awesome"),//"6537205da59c72b57ed3881843c2d24103d683a3"
+)
